dfaa3f3e4e3aa3d4861765c00093b92e716339cd
kusheen8 <kusheendhar942@gmail.com>
2025-07-25 13:44:57 +0530

    Switched to bcryptjs for Render compatibility

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from "validator"
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs';
 import { v2 as cloudinary } from 'cloudinary'
 import teacherModel from '../models/teacherModel.js'
 import uploadToCloudinary from "../config/cloudinary.js"
