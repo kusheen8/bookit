743028e9f3021534380fdc21c45a67f57dc04c79
KUSHEEN Dhar <kusheendhar942@gmail.com>
2025-07-25 15:37:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
+  server: { port: 5173 },
+  build: {
+    outDir: 'dist',
+  },
+  base: './', // 👈 required for relative assets in deployed environment
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
